c0d7512dde5c4c4b5096e0712d65f23b1c0c65d8
colo1211 <colo1211@naver.com>
2021-09-09 09:44:15 +0900

    bcrypt 를 활용한 pw 암호화 처리

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); 
+const saltRounds = 10; 
 
 const userSchema = mongoose.Schema({
     name : {
@@ -28,6 +30,23 @@ const userSchema = mongoose.Schema({
     }, 
     tokenExp : { // 토큰 유효기간 설정
         type : Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this; 
+    if (user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if (err) return next(err); 
+            // user.password는 암호처리 전의 비밀번호 
+            // hash 는 암호처리 후의 비밀번호
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if (err) return next(err); 
+                user.password = hash; // hash(암호화)된 비밀번호로 변경
+                next(); 
+            }
+        )
+        })
     }
 })
 
