805f5f74ef0b0044822c6e86e9708d64a2735dc2
colo1211 <colo1211@naver.com>
2021-09-10 15:00:58 +0900

    login

--- models/User.js
+++ models/User.js
@@ -1,55 +1,102 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); 
-const saltRounds = 10; 
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
-    name : {
-        type : String, 
-        maxlength : 50
+    name: {
+        type: String,
+        maxlength: 50
     },
-    email :{
-        type : String, 
-        trim : true, // 공백 자동 제거  
-        unique : 1, // 중복 X 
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
     },
-    password : {
-        type : String, 
-        minlength : 5
-    }, 
-    lastaname : {
-        maxlength : 50, 
-        type : String
+    password: {
+        type: String,
+        minlength: 5
     },
-    role : { // 1이면 관리자, 0이면 일반 사용자 
-        type : Number ,
-        default : 0
+    lastname: {
+        type: String,
+        maxlength: 50
     },
-    image : String, 
-    token : {
-        type : String
-    }, 
-    tokenExp : { // 토큰 유효기간 설정
-        type : Number
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
     }
 })
 
-userSchema.pre('save', function(next){
-    var user = this; 
-    if (user.isModified('password')){
-        bcrypt.genSalt(saltRounds, function(err, salt){
-            if (err) return next(err); 
-            // user.password는 암호처리 전의 비밀번호 
-            // hash 는 암호처리 후의 비밀번호
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if (err) return next(err); 
-                user.password = hash; // hash(암호화)된 비밀번호로 변경
-                next(); 
-            }
-        )
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
         })
+    } else {
+        next()
     }
 })
 
-const User = mongoose.model('User', userSchema); 
 
-module.exports = {User}
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    // console.log('user._id', user._id)
+
+    // jsonwebtoken을 이용해서 token을 생성하기 
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token 
+    // -> 
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    // user._id + ''  = token
+    //토큰을 decode 한다. 
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에 
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
