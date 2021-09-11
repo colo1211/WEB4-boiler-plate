ea3bfeeb239456a2f65bbca88d7441c8b91e5db8
colo1211 <colo1211@naver.com>
2021-09-11 17:16:10 +0900

    App.js -> Route 역할

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'; 
+import NavBar from './components/views/NavBar/NavBar'; 
+import LandingPage from './components/views/LandingPage/LandingPage'; 
+import LoginPage from './components/views/LoginPage/LoginPage'; 
+import RegisterPage from './components/views/RegisterPage/RegisterPage'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar/>
+          <Switch>
+            <Route exact path="/">
+              <LandingPage />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/register">
+              <RegisterPage /> 
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
+
+
 
 export default App;
