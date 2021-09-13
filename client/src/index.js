57583f8b02b229c2810e913f44edef03bea348de
colo1211 <colo1211@naver.com>
2021-09-13 16:42:57 +0900

    Redux MiddleWare install & setting

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// CSS 라이브러리 import 
 import 'antd/dist/antd.css';
+
+// Redux 라이브러리 import 4개
+import { Provider } from 'react-redux'; 
+import { applyMiddleware, createStore } from 'redux'; // middleware 연결
+import promiseMiddleWare from 'redux-promise'; // redux-store 에 Promise를 보낼 수 있는 middleWare 
+import ReduxThunk from 'redux-thunk'; // redux-store 에 Function 을 보낼 수 있는 middleWare
+
+import rootReducer from './_reducers';
+
+// 원래는 createStore 만 하면 되지만, 미들웨어 사용을 위해서 applyMiddleWare 를 적용
+const createStoreWithMiddleWare = applyMiddleware(promiseMiddleWare,ReduxThunk)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {createStoreWithMiddleWare(rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && 
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
