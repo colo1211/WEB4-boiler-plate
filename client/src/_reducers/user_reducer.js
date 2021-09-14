64a369b5ae9280499d0d2dc8cd87869ef35c0edc
colo1211 <colo1211@naver.com>
2021-09-14 14:05:13 +0900

    Reducer Register

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER } from "../_actions/types";
+import { LOGIN_USER, REGISTER_USER, LOGOUT_USER} from "../_actions/types";
 
 // type 에 따라 다른 조치를 취해야 하므로 switch 문으로 작성
 
@@ -6,10 +6,18 @@ import { LOGIN_USER } from "../_actions/types";
 
 export default function (state = {}, action){
     switch (action.type) {
+
+        // action.type 이 login_user 라면, 업데이트된 State 리턴
         case LOGIN_USER:
-            return { ...state, loginSuccess : action.payload}
+            return {...state, loginSuccess : action.payload}
             break;
+
+        case REGISTER_USER : 
+            return {...state, success : action.payload}
+            break; 
     
+  
+        // action.type 이 비어있다면, 초기상태의 state를 리턴
         default:
             return state;
     }
