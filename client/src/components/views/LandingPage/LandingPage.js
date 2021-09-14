ea453c2cf23923e7c110f4403940211bf62aa397
colo1211 <colo1211@naver.com>
2021-09-14 14:04:14 +0900

    Sign Out & Axios

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'; 
+import { useDispatch } from 'react-redux'; 
+import { logoutUser } from '../../../_actions/user_action'
+import { useHistory } from 'react-router';
 
 function LandingPage() {
 
+    const history = useHistory(); 
+    const dispatch = useDispatch(); 
+
+
+    // LogOut은 Redux 에 올릴 정보가 없으므로 이벤트리스너를 통해서 server/index.js 내 의 
+    // /api/users/logout 과 통신하여 success:true 를 받아오면 로그아웃 되도록 한다. 
+    
+    const onClickHandler = ()=> {
+        axios.get('/api/users/logout')
+        .then((response)=>{
+            if(response.data.success){
+                history.push('/login'); 
+            }else {
+                alert('Sign Out Failed'); 
+            }
+        })
+    }
 
     useEffect(()=>{
         axios.get('/api/hello')
         .then((response)=>{ 
-             console.log(response.data);     
+            //  console.log(response.data);     
         });
+        
     },[]); 
+
 
     return (
         <div style = {{
@@ -17,6 +39,8 @@ function LandingPage() {
             width : '100%', height : '100vh'
         }}>
            <h2>시작 페이지</h2>
+           <button onClick={ onClickHandler }> Sign Out </button>
+
         </div>
     )
 }
