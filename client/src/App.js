3667aebb44fe255f89b06ae13fc4a1a5c0275e51
colo1211 <colo1211@naver.com>
2021-09-14 19:51:09 +0900

    HOC Routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/views/NavBar/NavBar';
 import LandingPage from './components/views/LandingPage/LandingPage'; 
 import LoginPage from './components/views/LoginPage/LoginPage'; 
 import RegisterPage from './components/views/RegisterPage/RegisterPage'; 
+import Auth from './hoc/auth';
 
 function App() {
   return (
@@ -11,15 +12,13 @@ function App() {
       <div>
         <NavBar/>
           <Switch>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <Route path="/register">
-              <RegisterPage /> 
-            </Route>
+            {/*(option)*/}
+            {/*null : 아무나 출입이 가능한 페이지, Landing Page*/}
+            {/*true : 로그인한 유저만 출입이 가능한 페이지*/}
+            {/*false : 로그인한 유저 출입 불가 페이지*/}
+            <Route exact path="/" component = {Auth(LandingPage,null )}/>
+            <Route path="/login" component = {Auth(LoginPage,false )}/>
+            <Route path="/register" component = {Auth(RegisterPage, false )}/>
           </Switch>
       </div>
     </Router>
