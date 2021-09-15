21714dca0d86a3d6952081d6d9fa1268579add46
colo1211 <colo1211@naver.com>
2021-09-15 13:01:22 +0900

    Exact Path, HOC

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'; 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 import NavBar from './components/views/NavBar/NavBar'; 
 import LandingPage from './components/views/LandingPage/LandingPage'; 
 import LoginPage from './components/views/LoginPage/LoginPage'; 
@@ -12,19 +12,17 @@ function App() {
       <div>
         <NavBar/>
           <Switch>
-            {/*(option)*/}
+            {/*HOC Auth 컴포넌트 -> 2번째 인자 option*/}
             {/*null : 아무나 출입이 가능한 페이지, Landing Page*/}
             {/*true : 로그인한 유저만 출입이 가능한 페이지*/}
             {/*false : 로그인한 유저 출입 불가 페이지*/}
-            <Route exact path="/" component = {Auth(LandingPage,null )}/>
-            <Route path="/login" component = {Auth(LoginPage,false )}/>
-            <Route path="/register" component = {Auth(RegisterPage, false )}/>
+            <Route exact path="/" component = {Auth(LandingPage,null)}/>
+            <Route exact path="/login" component = {Auth(LoginPage,false)}/>
+            <Route exact path="/register" component = {Auth(RegisterPage, false)}/>
           </Switch>
       </div>
     </Router>
   );
 }
-
-
 
 export default App;
